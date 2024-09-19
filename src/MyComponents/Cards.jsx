fcdaa93f2f283f4250be7b4dbdcf672272eb4bfb
MyGithub-art <sangamwad@gmail.com>
2024-09-19 20:15:32 +0545

    commit

--- src/MyComponents/Cards.jsx
+++ src/MyComponents/Cards.jsx
@@ -5,10 +5,10 @@ export default function Cards(props){
         <section className="content">
             <img className="main-photo" src={`../images/${props.coverImg}`}/>
             <div className="photo-details">
-            <span className="bold"><img className="location-indicator" src="../images/location-indicator.png"/>{props.country}</span>
-            <a  className="gap" target="_blank" href={props.Maplink}>View in google maps</a>
-            <h2>{props.title}</h2>
-            <p className="bold ">{props.date.start} - {props.date.end}</p>
+            <span className="bold down"><img className="location-indicator" src="../images/location-indicator.png"/>{props.country}</span>
+            <a  className="gap down" target="_blank" href={props.Maplink}>View in google maps</a>
+            <h2 className="title">{props.title}</h2>
+            <p className="bold down">{props.date.start} - {props.date.end}</p>
             <div className="description">
             <p>{props.description}</p>
             </div>
