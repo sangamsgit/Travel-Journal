bebc2684cb104da0c57a87859cd596a8ef572512
MyGithub-art <sangamwad@gmail.com>
2024-09-19 17:47:27 +0545

    RWD

--- src/MyComponents/Cards.jsx
+++ src/MyComponents/Cards.jsx
@@ -8,8 +8,10 @@ export default function Cards(props){
             <span className="bold"><img className="location-indicator" src="../images/location-indicator.png"/>{props.country}</span>
             <a  className="gap" target="_blank" href={props.Maplink}>View in google maps</a>
             <h2>{props.title}</h2>
-            <p className="bold">{props.date.start} - {props.date.end}</p>
+            <p className="bold ">{props.date.start} - {props.date.end}</p>
+            <div className="description">
             <p>{props.description}</p>
+            </div>
             </div>
         </section>
     )
